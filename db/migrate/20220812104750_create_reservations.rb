1f3906486d27b4f097de2df255ba80013733f847
samiullah <bahadursamiullah@gmail.com>
2022-08-12 16:01:26 +0430

    update reservation

--- db/migrate/20220812104750_create_reservations.rb
+++ db/migrate/20220812104750_create_reservations.rb
@@ -1,7 +1,9 @@
 class CreateReservations < ActiveRecord::Migration[7.0]
   def change
     create_table :reservations do |t|
-
+      t.date :datetime
+      t.belongs_to :user, index: true, foreign_key: true
+      t.belongs_to :doctor, index: true, foreign_key: true
       t.timestamps
     end
   end
