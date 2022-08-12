fe141107dfc870b1f5c5a3d9410ec2bb8f74024a
samiullah <bahadursamiullah@gmail.com>
2022-08-12 16:01:08 +0430

    update doctor migrate

--- db/migrate/20220812104445_create_doctors.rb
+++ db/migrate/20220812104445_create_doctors.rb
@@ -1,7 +1,10 @@
 class CreateDoctors < ActiveRecord::Migration[7.0]
   def change
     create_table :doctors do |t|
-
+       t.string :name
+       t.string :speciality
+       t.string  :city
+       t.belongs_to :user, index: true, foreign_key: true
       t.timestamps
     end
   end
