ebe82f81de5e834f60d289c14ceb98da7ed65a73
samiullah <bahadursamiullah@gmail.com>
2022-08-13 15:39:13 +0430

    update doctor model

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,6 +1,6 @@
 class Doctor < ApplicationRecord
   has_many :reservations
-  has_many :users, through: :reservations
+  has_belongs_to :user
   validates :name, presence: true
   validates :specialty, presence: true
 end
