e84db7bdcc6ee308f9d7870dd0d2c24611cffce7
moise <moisemlg90@gmail.com>
2022-08-13 11:19:14 +0200

    Update the user devise and the database

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,20 @@
-class ApplicationController < ActionController::API
-    before_action :authenticate_user!
+class ApplicationController < ActionController::Base
+    before_action :update_allowed_parameters, if: :devise_controller?
+
+  def update_allowed_parameters
+    devise_parameter_sanitizer.permit(:sign_up) do |u|
+      u.permit(:firstname, :lastname, :email, :password, :password_confirmation)
+    end
+  end
+
+  private
+
+  def authenticate_user!
+    if user_signed_in?
+      super
+    else
+      redirect_to splashs_index_path
+    end
+  end
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'reservations/index'
-  get 'reservations/show'
-  get 'reservations/update'
-  get 'reservations/delete'
-  get 'doctors/index'
-  get 'doctors/show'
-  get 'doctors/update'
-  get 'doctors/delete'
-  get 'users/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "doctors#index"
 end
