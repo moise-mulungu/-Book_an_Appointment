445895aec4fe16eea12d1ac12b02671346678c14
samiullahbhadur <bahadursamiullah@gmail.com>
2022-08-15 01:27:15 +0430

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -35,8 +35,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_12_104750) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
-    t.string "email"
-    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
