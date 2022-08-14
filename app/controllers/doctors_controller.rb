bd2f9f516ebe3c4338fd5c3cae0612a87ede9da1
samiullah <bahadursamiullah@gmail.com>
2022-08-14 16:16:11 +0430

    Add index  action  in doctor  controller

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -4,49 +4,6 @@ class DoctorsController < ApplicationController
    def index
     @doctor=Doctor.all
    end
-  def new
-    @doctor=Doctor.new
-  end
-  def create
-   @doctor=Doctor.new(Doctor_params)
-   if @doctor.save
-    redirect_to doctor_path(@doctor)
-   else
-    render 'new'
-   end
-  end
-
-  def edit 
- 
-  end
-
-  def update 
-  
-    if @doctor.update( Doctor_params)
-      redirect_to doctors_path
-    else
-      render 'edit'
-    end
-  end
-
-  def show 
-  
-  end 
-
-  def destroy 
-    @doctor =Doctor.find(params[:id])
-    @doctor.destroy
-    redirect_to doctors_path
-  end
-
-  def set_Doctor 
-    @doctor= Doctor.find(params[:id])
-  end
-  private 
-  def Doctor_params
-     params.require(:doctor).permit(:name, :speciality, :user_id)
-  end
-
  
   
 end
