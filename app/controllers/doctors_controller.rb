ffbc2d9da4de2a07507d835611c175a4b0077005
samiullah <bahadursamiullah@gmail.com>
2022-08-14 16:11:03 +0430

    Update gemfile

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -1,11 +1,52 @@
 class DoctorsController < ApplicationController
-  def index
-    @doctors = Doctor.all
+  before_action :set_Doctor, only:[:edit, :update, :show]
+    
+   def index
+    @doctor=Doctor.all
+   end
+  def new
+    @doctor=Doctor.new
+  end
+  def create
+   @doctor=Doctor.new(Doctor_params)
+   if @doctor.save
+    redirect_to doctor_path(@doctor)
+   else
+    render 'new'
+   end
   end
 
-  def show; end
+  def edit 
+ 
+  end
 
-  def update; end
+  def update 
+  
+    if @doctor.update( Doctor_params)
+      redirect_to doctors_path
+    else
+      render 'edit'
+    end
+  end
 
-  def delete; end
+  def show 
+  
+  end 
+
+  def destroy 
+    @doctor =Doctor.find(params[:id])
+    @doctor.destroy
+    redirect_to doctors_path
+  end
+
+  def set_Doctor 
+    @doctor= Doctor.find(params[:id])
+  end
+  private 
+  def Doctor_params
+     params.require(:doctor).permit(:name, :speciality, :user_id)
+  end
+
+ 
+  
 end
--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,9 +1,37 @@
 class ReservationsController < ApplicationController
-  def index; end
+  before_action :set_reservation, only:[:edit, :update, :show]
+  def index
+    @reservation=Reservation.all
+   end
 
-  def show; end
+  def show
+     end
 
-  def update; end
+     def edit
+     end
 
-  def delete; end
+  def update
+    if @reservation.update(reservation_params)
+      redirect_to reservations_path
+    else
+      render 'edit'
+    end
+   end
+
+  def create
+     @reservation=Reservation.new(reservation_params)
+     if @reservation.save 
+      redirect_to reservations_path
+   end
+
+
+  def set_reservation
+    @reservation=Reservation.find(params[:id])
+  end
+
+   private
+   def reservation_params
+    params.require(:reservation).permit(:datetime, :user_id, :doctor_id)
+   end
+
 end
