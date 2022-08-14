be0d4606605b9bc1503b884861cb9caf60379a12
samiullah <bahadursamiullah@gmail.com>
2022-08-14 16:19:52 +0430

    Add  index  action  in reservation  controller

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -4,34 +4,5 @@ class ReservationsController < ApplicationController
     @reservation=Reservation.all
    end
 
-  def show
-     end
-
-     def edit
-     end
-
-  def update
-    if @reservation.update(reservation_params)
-      redirect_to reservations_path
-    else
-      render 'edit'
-    end
-   end
-
-  def create
-     @reservation=Reservation.new(reservation_params)
-     if @reservation.save 
-      redirect_to reservations_path
-   end
-
-
-  def set_reservation
-    @reservation=Reservation.find(params[:id])
-  end
-
-   private
-   def reservation_params
-    params.require(:reservation).permit(:datetime, :user_id, :doctor_id)
-   end
 
 end
