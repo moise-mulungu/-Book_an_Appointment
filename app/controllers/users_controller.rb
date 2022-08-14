d5a2528fff2661e1a1ec4007f837f3d154d4bb7f
samiullah <bahadursamiullah@gmail.com>
2022-08-14 16:13:45 +0430

    Add create  action  in user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,18 @@ class UsersController < ApplicationController
     @user=User.new
   end
 
+ def create
+   @user=User.new(user_params)
+   if @user.save
+    ## when user created and direct to users profile page
+    session[:user_id]=@user.id
+
+    redirect_to user_path(@user), notice:'You signup successfully!'
+   else
+    render 'new'
+   end
+  end
+
+
+ 
 end
