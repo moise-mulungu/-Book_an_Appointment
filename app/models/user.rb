844d8584f420f7c2342dca166dccf747b278c2a9
Cédric Kossi <lionel.kossi@gmail.com>
2022-08-16 15:45:52 +0000

    Fix models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,6 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  # devise :database_authenticatable, :registerable,
-  #        :recoverable, :rememberable, :validatable, :confirmable
-  # has_secure_password
-
   has_many :reservations
-  has_many :doctors
+  has_many :doctors, through: :reservations
 
   validates :username, presence: true, uniqueness: true
-  # VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  # validates :email , presence: true, uniqueness: true, length: { maximum: 100 },
-  #  format: { with: VALID_EMAIL_REGEX }
 end
