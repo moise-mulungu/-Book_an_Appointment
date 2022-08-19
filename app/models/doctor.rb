5623ed459476b289f10307a70b4e1b6bfc1852f5
moise <moisemlg90@gmail.com>
2022-08-19 13:43:13 +0200

    Update validations for the doctor model

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -4,4 +4,8 @@ class Doctor < ApplicationRecord
   has_many :users, through: :reservations
   validates :name, presence: true
   validates :speciality, presence: true
+  validates :bill, presence: true
+  validates :image, presence: true
+  validates :location, presence: true
+  validates :email, presence: true
 end
