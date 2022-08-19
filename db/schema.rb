ecc1b61c3d44b01a5c2e5ef5540d57524966688f
samiullahbhadur <bahadursamiullah@gmail.com>
2022-08-19 09:59:28 +0430

    update schema and update gem

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_12_104750) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_19_050227) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -29,6 +29,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_12_104750) do
     t.bigint "doctor_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "city"
     t.index ["doctor_id"], name: "index_reservations_on_doctor_id"
     t.index ["user_id"], name: "index_reservations_on_user_id"
   end
