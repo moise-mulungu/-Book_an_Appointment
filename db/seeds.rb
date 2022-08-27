c841f1a1bdd1379de0058aa0b887a0f0542539b6
moise <moisemlg90@gmail.com>
2022-08-27 21:56:27 +0200

    Resolve some conflicts

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,3 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-# ced = User.create(username: "bla")
-# doc = Doctor.create(name: "Doc", speciality: "Orthi", bill: "1000", image: "Love", location: "Togo", email: "Email@gmail.com", user: ced)
